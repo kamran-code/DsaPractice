c8fec2ec176fe582d5ea808592da1169c2c18a28
Kamran Khan <kkhan2928@gmail.com>
2025-03-23 14:54:45 +0530

    PascleTriangle

--- Arrays/MaxConsecutiveOnes.java
+++ Arrays/MaxConsecutiveOnes.java
@@ -1,36 +1,7 @@
-import java.util.Scanner;
-import java.util.ArrayList;
 
 public class MaxConsecutiveOnes {
 
     public static void main(String args[]) {
-        Scanner obj = new Scanner(System.in);
-        int numRows = obj.nextInt();
-        ArrayList<ArrayList<Integer>> answer = new ArrayList<>();
 
-        ArrayList<Integer> temp = new ArrayList<>();
-        temp.add(1);
-        answer.add(new ArrayList<>(temp));
-        for (int i = 1; i < numRows; i++) {
-            ArrayList<Integer> arr = new ArrayList<>();
-            arr.add(1);
-            if (i >= 2) {
-                for (int j = 1; j <= i - 1; j++) {
-                    arr.add(answer.get(i - 1).get(j - 1) + answer.get(i - 1).get(j));
-                }
-            }
-            arr.add(1);
-            answer.add(new ArrayList<>(arr));
-        }
-        for(int i =0;i<numRows;i++){
-
-            System.out.println(answer.get(i));
-        }
-
-        obj.close();
     }
 }
-// 1
-// 1 1
-// 1 2 1
-// 1 3 3 1
--- Arrays/PascalTriangle.java
+++ Arrays/PascalTriangle.java
@@ -1,6 +1,33 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class PascalTriangle {
     
     public static void main(String args[]) {
 
+          Scanner obj = new Scanner(System.in);
+        int numRows = obj.nextInt();
+        ArrayList<ArrayList<Integer>> answer = new ArrayList<>();
+
+        ArrayList<Integer> temp = new ArrayList<>();
+        temp.add(1);
+        answer.add(new ArrayList<>(temp));
+        for (int i = 1; i < numRows; i++) {
+            ArrayList<Integer> arr = new ArrayList<>();
+            arr.add(1);
+            if (i >= 2) {
+                for (int j = 1; j <= i - 1; j++) {
+                    arr.add(answer.get(i - 1).get(j - 1) + answer.get(i - 1).get(j));
+                }
+            }
+            arr.add(1);
+            answer.add(new ArrayList<>(arr));
+        }
+        for(int i =0;i<numRows;i++){
+
+            System.out.println(answer.get(i));
+        }
+
+        obj.close();
     }
 }
